313f359593cd3d1f2094f591961ff24faecfcdb8
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-10-21 16:29:07 +0200

    second task

--- 06_Day_Map_List_Keys/06_map_list_keys_boilerplate/src/index.js
+++ 06_Day_Map_List_Keys/06_map_list_keys_boilerplate/src/index.js
@@ -1,49 +1,87 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
+import React from "react";
+import ReactDOM from "react-dom";
 
 // importing data
 
-import { countriesData } from './data/countries'
-import { tenMostHighestPopulations } from './data/ten_most_highest_populations'
+import { tenHighestPopulation } from "./data/ten_most_highest_populations";
+import css from "../src/index.module.css";
 
-const countries = [
-  { name: 'Finland', city: 'Helsinki' },
-  { name: 'Sweden', city: 'Stockholm' },
-  { name: 'Denmark', city: 'Copenhagen' },
-  { name: 'Norway', city: 'Oslo' },
-  { name: 'Iceland', city: 'Reykjavík' },
-]
-
-// Country component
-const Country = ({ country: { name, city } }) => {
+const Country = ({ population: { country, population } }) => {
   return (
-    <div>
-      <h1>{name}</h1>
-      <small>{city}</small>
-    </div>
-  )
+    <ul>
+      <li className={css.listEl}>
+        {country}
+        <p style={{ textTransform: "uppercase" }}>
+          {population.toLocaleString()}
+        </p>
+      </li>
+    </ul>
+  );
+};
+
+const Countries = ({ populations }) => {
+  const countryList = populations.map((population) => (
+    <Country population={population} />
+  ));
+  return <div className={css.listWrapper}>{countryList}</div>;
+};
+
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
 }
 
-// countries component
-const Countries = ({ countries }) => {
-  const countryList = countries.map((country) => (
-    <Country key={country.name} country={country} />
-  ))
-  return <div>{countryList}</div>
-}
+const Table = ({ numbers }) => {
+  const list = numbers.map((number) => {
+    const isPrimeNumber = isPrime(number);
+
+    if (isPrimeNumber) {
+      return (
+        <li key={number} className={`${css.prime} ${css.listElement}`}>
+          {number}
+        </li>
+      );
+    }
+    if (number % 2 === 0) {
+      return (
+        <li key={number} className={`${css.even} ${css.listElement}`}>
+          {number}
+        </li>
+      );
+    } else if (number % 2 !== 0) {
+      return (
+        <li key={number} className={`${css.odd} ${css.listElement}`}>
+          {number}
+        </li>
+      );
+    }
+  });
+  return <ul className={css.list}>{list}</ul>;
+};
 
 // The App, or the parent or the container component
 // Functional Component
 const App = () => {
+  const numbers = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+  ];
   return (
-    <div className='app'>
+    <div className="app">
       <div>
-        <h1>Countries List</h1>
-        <Countries countries={countries} />
+        <h1 className={css.header}>World population</h1>
+        <p className={css.paragraph}>Ten most populated countries</p>
+        <Countries populations={tenHighestPopulation} />
+      </div>
+      <div>
+        <h1 className={css.header}>Number Generator</h1>
+        <Table numbers={numbers} />
       </div>
     </div>
-  )
-}
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+  );
+};
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
